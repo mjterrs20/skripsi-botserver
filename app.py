92b97491ccc8d32ecc03d6f3a712cd8c9b22ecd5
mjterrs20 <aditia20.riz@gmail.com>
2021-08-21 16:38:13 +0700

    ternyata begini anjing

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
+import numpy as np
 from flask import Flask, request, jsonify
 
+bilangan = [10,11,12,18,20]
+rizki ="testing aja"
+npScore = np.array(bilangan)
+maxScore = np.amax(npScore)
 
 app = Flask(__name__)
 
@@ -20,3 +25,7 @@ def login():
         })
     except KeyError as e:
         return str(e)
+
+@app.route("/test")
+def test():
+    return str(maxScore)
