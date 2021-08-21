da37ba4d54ee410d044c3eabfac84693956a874b
mjterrs20 <aditia20.riz@gmail.com>
2021-08-21 16:22:41 +0700

    add and test numpy

--- app.py
+++ app.py
@@ -1,4 +1,11 @@
+import numpy as np
 from flask import Flask, request, jsonify
+
+bilangan = [10,11,12,18,20]
+rizki ="testing aja"
+npScore = np.array(bilangan)
+maxScore = np.amax(npScore)
+
 
 app = Flask(__name__)
 
@@ -19,3 +26,7 @@ def login():
         })
     except KeyError as e:
         return str(e)
+
+@app.route("/test")
+def test():
+    return str(maxScore)
