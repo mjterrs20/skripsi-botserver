9ecfcdb3cb97628ce426261cf4c7bf49155ace75
mjterrs20 <aditia20.riz@gmail.com>
2021-08-22 13:42:25 +0700

    testing

--- app.py
+++ app.py
@@ -1,19 +1,9 @@
-from rake_nltk import Rake
-from keras.models import load_model
 from flask import Flask, request, jsonify, render_template
 
 
 app = Flask(__name__)
 
 
-
-# load cnn model
-model = load_model("cnn_model.h5")
-
-
-
-# install Rake
-r = Rake(language="indonesian", min_length=1, max_length=5)
 
 # route duniawi
 @app.route("/")
@@ -25,10 +15,7 @@ def hello_world():
 @app.route("/rake", methods= ['POST'])
 def login():
     try: 
-        r.extract_keywords_from_text(request.form['quest'])
-        keyword = r.get_ranked_phrases()
-        print(keyword)
-        return str(keyword)
+        return request.form['quest']
     except KeyError as e:
         return str(e)
 
