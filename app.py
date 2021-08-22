fdbef8c35a79bf25287041fd748bbf09e00c00b6
mjterrs20 <aditia20.riz@gmail.com>
2021-08-22 22:22:27 +0700

    complete cnn

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import json
 from flask import Flask, request, jsonify, render_template
 from keras.models import load_model
 from keras.preprocessing.text import Tokenizer
@@ -5,13 +6,20 @@ from keras.preprocessing.sequence import pad_sequences
 
 app = Flask(__name__)
 
+# load data using Python JSON module
+with open('chatbot_eksyar.json','r') as f:
+    data = json.loads(f.read())
+
 # load cnn model
 model = load_model("cnn_model.h5")
+labels = ['asuransi', 'bank', 'eksyar', 'investasi', 'reksadana']
 
 # Tokonizer
-corpus = ["Bagaimana ketentuan istishna?", "Apa yang dimaksud ijarah?", "Bagaimana cara kerja ijarah?"]
+datas = []
+for i in range(len(data['items'])):
+    datas.append(data['items'][i]["questions"])
 tokenizer = Tokenizer()
-tokenizer.fit_on_texts(corpus)
+tokenizer.fit_on_texts(datas)
 
 # route duniawi
 @app.route("/")
@@ -20,11 +28,13 @@ def hello_world():
 
 
 
-@app.route("/rake", methods= ['POST'])
-def login():
+@app.route("/chat", methods= ['POST'])
+def question():
     try: 
-        res = cnn_predict(request.form['quest'])
-        return res 
+        label = cnn_predict(request.form['quest'])
+        return jsonify({
+            'label' : label,
+        })
     except KeyError as e:
         return str(e)
 
@@ -32,4 +42,5 @@ def cnn_predict(quest):
     puretext = tokenizer.texts_to_sequences([quest])
     text_pad = pad_sequences(puretext,maxlen=50,padding='post')
     predicted = model.predict(text_pad)
-    return str(predicted)
+    index_label = predicted.argmax()
+    return labels[index_label]
